bcf09ab385704f73d1ee7f3124e908d701d5f772
Saiyara <116515386+Saiyara-Islam-123@users.noreply.github.com>
2024-09-08 17:09:02 -0400

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -1,48 +1,41 @@
-#loading dataset
-import numpy as np
-import pandas as pd
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 import torch
 
-
-df = pd.read_csv('mnist_train.csv')
-df_without_labels = df.drop(columns = "label")
-
-#It has columns for pixels. I'll make into one nice matrix. So the resulting df will have 2 columns
-
-
-matrix = []
-matrices = []
-labels = []
-
-   
-for i in range(10000): #row wise. I'm taking a smaller sample
-    for column in df_without_labels: #column wise
-        column_name = column.split("x")
-        matrix.append(df_without_labels[column][i])
-        
-            
-    #df_main = df_main.append( {"Label" : df["label"][i], "Matrix" : matrix}, ignore_index=True ) 
-    matrices.append(matrix)
-    labels.append(df["label"][i])
+train_data = datasets.MNIST(
     
-
-    matrix = []
+    root = "data",
+    train = True,
+    transform = ToTensor(),
+    download = True
     
     
+    )
 
-#############################################################################################################
-
-#df_unsupervised = df_main.drop(columns = "Label")
-#df_supervised = df_main.copy()
-
-
-
-matrices_numpy = np.array(matrices)
-input_tensor = torch.tensor(matrices_numpy, dtype=torch.float32)
+test_data = datasets.MNIST(
+    
+    root = "data",
+    train = False,
+    transform = ToTensor(),
+    download = True
+    
+    
+    )
 
 def get_inputs():
-    return input_tensor
+    
+    images = torch.stack([train_data[i][0] for i in range(len(train_data))])
+    
+    return images.view(images.size(0), -1)
 
 def get_labels():
-    return torch.tensor(labels)
+    return torch.tensor([train_data[i][1] for i in range(len(train_data))])
 
+
+def get_test_inputs():
+    images = torch.stack([test_data[i][0] for i in range(len(test_data))])
+    
+    return images.view(images.size(0), -1)
+
+def get_test_labels():
+    return torch.tensor([test_data[i][1] for i in range(len(test_data))])
