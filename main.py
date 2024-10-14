17f6b782da6a15b1efda7aa822a0ea083402dae6
Saiyara <saiyaraislam17@gmail.com>
2024-10-14 17:49:40 -0400

    3 line graphs for within and between

--- main.py
+++ main.py
@@ -12,17 +12,16 @@ if __name__ == "__main__":
     X = create_dataset.get_inputs()
     y = create_dataset.get_labels()
 
-    #pair_avg_distances = {}
-    #pair_avg_distances[(4,4)] = [sampled_avg_distance((4, 4), X , y)]
-    #pair_avg_distances[(4, 9)] = [sampled_avg_distance((4, 9), X, y)]
+    pair_avg_distances = {}
+    pair_avg_distances[(1,1)] = [sampled_avg_distance((1, 1), X , y)]
+    pair_avg_distances[(1, 0)] = [sampled_avg_distance((1, 0), X, y)]
+    pair_avg_distances[(0, 0)] = [sampled_avg_distance((0, 0), X, y)]
 
     #X_filtered, y_filtered = plotting_X.filter(X, y, [1, 0, 4, 9])
 
     #print(X_filtered.shape)
 
     #plotting_X.plot(X_filtered.reshape(X_filtered.shape[0], 784), y_filtered, -1)
-
-
     
     model = neural_networks.AutoEncoder()
     loss_fn = nn.MSELoss()
@@ -53,11 +52,12 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
 
-        outputs_filtered, y_filtered = filter(torch.cat(outputs_list), y, [1, 0, 4, 9])
-        plot(outputs_filtered, y_filtered, epoch, "unsup")
+        #outputs_filtered, y_filtered = filter(torch.cat(outputs_list), y, [1, 0, 4, 9])
+        #plot(outputs_filtered, y_filtered, epoch, "unsup")
 
-        #pair_avg_distances[(4, 4)] = pair_avg_distances[(4, 4)] + [sampled_avg_distance((4, 4), torch.cat(outputs_list, dim=0), y)]
-        #pair_avg_distances[(4, 9)] = pair_avg_distances[(4, 9)] + [sampled_avg_distance((4, 9), torch.cat(outputs_list, dim=0), y)]
+        pair_avg_distances[(1, 1)] = pair_avg_distances[(1, 1)] + [sampled_avg_distance((1, 1), torch.cat(outputs_list, dim=0), y)]
+        pair_avg_distances[(1, 0)] = pair_avg_distances[(1, 0)] + [sampled_avg_distance((1, 0), torch.cat(outputs_list, dim=0), y)]
+        pair_avg_distances[(0, 0)] = pair_avg_distances[(0, 0)] + [sampled_avg_distance((0, 0), torch.cat(outputs_list, dim=0), y)]
             
         print(f"Epoch {epoch+1}, Loss: {loss.item()}")
 
@@ -68,11 +68,6 @@ if __name__ == "__main__":
     
     loss_fn_2 = nn.CrossEntropyLoss()
     optimizer_2 = optim.Adam(model.parameters(), lr=0.01)
-
-
-
-
-
 
     print("\nSupervised part!")
 
@@ -97,28 +92,30 @@ if __name__ == "__main__":
             loss.backward()
             optimizer_2.step()
 
-        outputs_sup_filtered, y_filtered_sup = filter(torch.cat(outputs_supervised_list), y, [1, 0, 4, 9])
-        plot(outputs_sup_filtered, y_filtered_sup, epoch, "sup")
+        #outputs_sup_filtered, y_filtered_sup = filter(torch.cat(outputs_supervised_list), y, [1, 0, 4, 9])
+        #plot(outputs_sup_filtered, y_filtered_sup, epoch, "sup")
 
         print(f"Epoch {epoch + 1}, Loss: {loss.item()}")
 
-        #pair_avg_distances[(4, 4)] = pair_avg_distances[(4, 4)] + [sampled_avg_distance((4, 4), torch.cat(outputs_supervised_list, dim=0), y)]
-        #pair_avg_distances[(4, 9)] = pair_avg_distances[(4, 9)] + [sampled_avg_distance((4, 9), torch.cat(outputs_supervised_list, dim=0), y)]
+        pair_avg_distances[(1, 1)] = pair_avg_distances[(1, 1)] + [sampled_avg_distance((1, 1), torch.cat(outputs_supervised_list, dim=0), y)]
+        pair_avg_distances[(1, 0)] = pair_avg_distances[(1, 0)] + [sampled_avg_distance((1, 0), torch.cat(outputs_supervised_list, dim=0), y)]
+        pair_avg_distances[(0, 0)] = pair_avg_distances[(0, 0)] + [sampled_avg_distance((0, 0), torch.cat(outputs_supervised_list, dim=0), y)]
+
+    within_1 = pair_avg_distances[(1,1)]
+    within_0 = pair_avg_distances[(0, 0)]
+    between = pair_avg_distances[(1,0)]
 
 
-    #within = pair_avg_distances[(4,4)]
-    #between = pair_avg_distances[(4,9)]
 
+    plt.plot(list(range(len(within_1))), within_1, label='Within 1', marker='o', color='green')  # First line with markers
+    plt.plot(list(range(len(within_0))), within_0, label='Within 0', marker='s',color='green')  # First line with markers
+    plt.plot(list(range(len(between))), between, label='Between 1 and 0', marker='x', color = "blue")  # Second line with different markers
 
-
-    #plt.plot(list(range(len(within))), within, label='Within 4', marker='o', color='green')  # First line with markers
-    #plt.plot(list(range(len(between))), between, label='Between 4 and 9', marker='x', color = "blue")  # Second line with different markers
-
-    #plt.xlabel("Phases")
-    #plt.ylabel("Distances")
-    #plt.title("Distance between 4 and 9 and within 4 before, during and after both training CNN")
-    #plt.savefig("Distance between 4 and 9 and within 4 before, during and after both training.png CNN.")
-    #plt.show()
+    plt.xlabel("Phases")
+    plt.ylabel("Distances")
+    plt.title("Distance between 1 and 0 and within 1 before, during and after both training CNN")
+    plt.savefig("Distance between 1 and 0 and within 1 before, during and after both training CNN trial 2.png")
+    plt.show()
     
 
     #measuring accuracy
