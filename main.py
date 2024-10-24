69f5dfca0e543e789d8a9206ea1a75da51fb3bd8
Saiyara <saiyaraislam17@gmail.com>
2024-10-24 00:49:39 -0400

    Update

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     y = create_dataset.get_labels()
 
     pair_avg_distances = {}
-    pair_avg_distances[(1,1)] = [sampled_avg_distance((1, 1), X , y)]
-    pair_avg_distances[(1, 0)] = [sampled_avg_distance((1, 0), X, y)]
-    pair_avg_distances[(0, 0)] = [sampled_avg_distance((0, 0), X, y)]
+    pair_avg_distances[(4,4)] = [sampled_avg_distance((4, 4), X , y)]
+    pair_avg_distances[(4, 9)] = [sampled_avg_distance((4, 9), X, y)]
+    pair_avg_distances[(9, 9)] = [sampled_avg_distance((9, 9), X, y)]
 
     #X_filtered, y_filtered = plotting_X.filter(X, y, [1, 0, 4, 9])
 
@@ -51,9 +51,9 @@ if __name__ == "__main__":
         #outputs_filtered, y_filtered = filter(torch.cat(outputs_list), y, [1, 0, 4, 9])
         #plot(outputs_filtered, y_filtered, epoch, "unsup")
 
-        pair_avg_distances[(1, 1)] = pair_avg_distances[(1, 1)] + [sampled_avg_distance((1, 1), torch.cat(outputs_list, dim=0), y)]
-        pair_avg_distances[(1, 0)] = pair_avg_distances[(1, 0)] + [sampled_avg_distance((1, 0), torch.cat(outputs_list, dim=0), y)]
-        pair_avg_distances[(0, 0)] = pair_avg_distances[(0, 0)] + [sampled_avg_distance((0, 0), torch.cat(outputs_list, dim=0), y)]
+        pair_avg_distances[(4, 4)] = pair_avg_distances[(4, 4)] + [sampled_avg_distance((4, 4), torch.cat(outputs_list, dim=0), y)]
+        pair_avg_distances[(4, 9)] = pair_avg_distances[(4, 9)] + [sampled_avg_distance((4, 9), torch.cat(outputs_list, dim=0), y)]
+        pair_avg_distances[(9, 9)] = pair_avg_distances[(9, 9)] + [sampled_avg_distance((9, 9), torch.cat(outputs_list, dim=0), y)]
             
         print(f"Epoch {epoch+1}, Loss: {loss.item()}")
 
@@ -92,26 +92,26 @@ if __name__ == "__main__":
 
         print(f"Epoch {epoch + 1}, Loss: {loss.item()}")
 
-        print(model_2.autoencoder_output.shape)
+        #print(model_2.autoencoder_output.shape)
 
-        pair_avg_distances[(1, 1)] = pair_avg_distances[(1, 1)] + [sampled_avg_distance((1, 1), torch.cat(outputs_autoencoder_list, dim=0), y)]
-        pair_avg_distances[(1, 0)] = pair_avg_distances[(1, 0)] + [sampled_avg_distance((1, 0), torch.cat(outputs_autoencoder_list, dim=0), y)]
-        pair_avg_distances[(0, 0)] = pair_avg_distances[(0, 0)] + [sampled_avg_distance((0, 0), torch.cat(outputs_autoencoder_list, dim=0), y)]
+        pair_avg_distances[(4, 4)] = pair_avg_distances[(4, 4)] + [sampled_avg_distance((4, 4), torch.cat(outputs_autoencoder_list, dim=0), y)]
+        pair_avg_distances[(4, 9)] = pair_avg_distances[(4, 9)] + [sampled_avg_distance((4, 9), torch.cat(outputs_autoencoder_list, dim=0), y)]
+        pair_avg_distances[(9, 9)] = pair_avg_distances[(9, 9)] + [sampled_avg_distance((9, 9), torch.cat(outputs_autoencoder_list, dim=0), y)]
 
-    within_1 = pair_avg_distances[(1,1)]
-    within_0 = pair_avg_distances[(0, 0)]
-    between = pair_avg_distances[(1,0)]
+    within_4 = pair_avg_distances[(4,4)]
+    within_9 = pair_avg_distances[(9, 9)]
+    between = pair_avg_distances[(4,9)]
 
 
 
-    plt.plot(list(range(len(within_1))), within_1, label='Within 1', marker='o', color='green')  # First line with markers
-    plt.plot(list(range(len(within_0))), within_0, label='Within 0', marker='s',color='green')  # First line with markers
-    plt.plot(list(range(len(between))), between, label='Between 1 and 0', marker='x', color = "blue")  # Second line with different markers
+    plt.plot(list(range(len(within_4))), within_4, label='Within 4', marker='o', color='green')  # First line with markers
+    plt.plot(list(range(len(within_9))), within_9, label='Within 9', marker='s',color='green')  # First line with markers
+    plt.plot(list(range(len(between))), between, label='Between 4 and 9', marker='x', color = "blue")  # Second line with different markers
 
     plt.xlabel("Phases")
     plt.ylabel("Distances")
-    plt.title("Distance between 1 and 0 and within 1 before, during and after both training CNN")
-    plt.savefig("Distance between 1 and 0 and within 1 before, during and after both training CNN trial 2.png")
+    plt.title("Distance between 4 and 9 and within 4 before, during and after both training CNN")
+    plt.savefig("Distance between 4 and 9 and within 4 before, during and after both training CNN trial 2.png")
     plt.show()
     
 
